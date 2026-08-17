01115ecbae80f27058248097e566518d8a04b7bd
Jax <piston.pro0001@gmail.com>
2026-08-17 07:51:17 -0600

    Made it actually function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask
 import threading
+import scratchattach as sa
+import time
+import random
+import os
 
 app = Flask(__name__)
 
@@ -8,10 +12,26 @@ def home():
     return "alive"
 
 def run_bot():
-    print("this estch a test")
     while True:
-        pass #placeholder
+        try:
+            session = sa.login(os.getenv("SCRATCH_USER"), os.getenv("SCRATCH_PASS"))
+            cloud = session.connect_cloud("1314420436")
+            while True:
+                print("Starting new phase...")
+                cloud.set_var("ready?", "0")
+                total_time = float(cloud.get_var("track-length")) #no need to check if it isdigit, will always be a number to two decimals
+                print(f"Song length is {total_time} secs")
+                print("Sleeping the song out.")
+                time.sleep(total_time)
+                cloud.set_var("song-#", str(random.randint(1, 2)))
+                cloud.set_var("ready?", "1")
+                print("Successfully restarted and chose new song!")
+                time.sleep(2)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            time.sleep(3)
 
-threading.Thread(target=run_bot).start()
+threading.Thread(target=run_bot, daemon=True).start()
 
 app.run(host="0.0.0.0", port=10000)
