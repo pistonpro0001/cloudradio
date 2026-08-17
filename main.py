4dd0bb094b113331e9d3e116cf66e89fd4c808a3
Jax <piston.pro0001@gmail.com>
2026-08-17 07:51:18 -0600

    I don't even know what I did

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["FLASK_ENV"] = "production"
-
 # --- SCRATCHATTACH BUG #608 ---
 original_process = sa.Session._process_session_id
 
@@ -26,7 +24,7 @@ def patched_process_session_id(self):
         else:
             raise e
 
-sa.Session._process_session_id = patched_process_session_id
+#sa.Session._process_session_id = patched_process_session_id
 
 app = Flask(__name__)
 
@@ -51,44 +49,61 @@ def run_bot():
         try:
             try:
                 session = sa.login(os.getenv("SCRATCH_USER"), os.getenv("SCRATCH_PASS"))
+                log("Managed to login via username and password.", "gold")
             except:
                 log("Couldn't login via username and password, using session id", "red")
                 session = sa.login_by_id(os.getenv("SC_SESS_ID"))
-            cloud = session.connect_cloud("1314420436")
+            
+            cloud = session.connect_cloud(project_id="1334822091")
             log("Cloud is ready", "lime")
             
             while True:
                 log("Starting new phase...")
                 cloud.set_var("ready", "0")
-                cur_song = cloud.get_var("song-#")
+                time.sleep(0.1)  # <-- Give it a split second to send
+                
+                cur_song = cloud.get_var("song_num")
                 if cur_song is None:
                     cur_song = random.randint(0, len(track_lengths)-1)
                     log("Could not get the song number, resetting it to a random number.", "red")
-                    cloud.set_var("song-#", str(cur_song+1))
+                    cloud.set_var("song_num", str(cur_song+1))
+                    log("It is now song " + str(cloud.get_var("song_num")))
+                    time.sleep(0.1) # <-- Give it a split second to send
+                    
                 total_time = track_lengths[int(cur_song)] * 100
                 cloud.set_var("tracklength", str(total_time))
+                time.sleep(0.1)  # <-- Give it a split second to send
+                
                 total_time /= 100
                 log(f"Song length is {total_time} secs")
                 log("Sleeping the song out.")
+                
                 start = time.time()
                 next_update = start + 1.0
+                last_progress = None
                 
                 while True:
                     elapsed = time.time() - start
+                    current_progress = str(round(elapsed))
                     
-                    cloud.set_var("progress", str(round(elapsed)))
+                    # Optimization: Only send if the value actually changed!
+                    if current_progress != last_progress:
+                        cloud.set_var("progress", current_progress)
+                        last_progress = current_progress
                     
                     if elapsed >= total_time:
                         break
                     
                     sleep_time = next_update - time.time()
                     if sleep_time > 0:
-                        jitter = random.uniform(0.01, 0.05)
+                        # Slightly increased jitter to keep Scratch's spam filters happy
+                        jitter = random.uniform(0.02, 0.08)
                         time.sleep(sleep_time + jitter)
                     
                     next_update += 1.0
                     
-                cloud.set_var("song-#", str(random.randint(1, len(track_lengths))))
+                cloud.set_var("song_num", str(random.randint(1, len(track_lengths))))
+                time.sleep(0.1) # <-- Safety sleep
                 cloud.set_var("ready", "1")
                 log("Successfully restarted and chose new song!")
                 time.sleep(2)
@@ -101,4 +116,4 @@ def run_bot():
             time.sleep(3)
 
 threading.Thread(target=run_bot, daemon=True).start()
-app.run(host="0.0.0.0", port=11303, debug=True, use_reloader=False)
+app.run(host="0.0.0.0", port=11303)
