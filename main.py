961b74a121754efdd6462e01357d5ca17ee6fb63
Jax <piston.pro0001@gmail.com>
2026-08-17 07:51:17 -0600

    Made le bot stay alive

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('this estch a test')
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "alive"
+
+def run_bot():
+    print("this estch a test")
+    while True:
+        pass #placeholder
+
+threading.Thread(target=run_bot).start()
+
+app.run(host="0.0.0.0", port=10000)
